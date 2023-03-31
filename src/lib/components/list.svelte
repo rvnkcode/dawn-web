42122db3231b4127cf2c5f7fae89aa54778ad40e
rvnkcode <sykim.p508@gmail.com>
2023-03-31 02:56:43 +0000

    make toggle completed using fetch patch api

--- src/lib/components/list.svelte
+++ src/lib/components/list.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import type { Task } from '@prisma/client';
 
+	const toggleCompleted = async (id: number, checked: boolean) => {
+		await fetch('/api', {
+			method: 'PATCH',
+			body: JSON.stringify({ id, checked })
+		});
+	};
+
 	export let tasks: Task[];
 </script>
 
@@ -9,7 +16,13 @@
 		<li>
 			<input type="checkbox" id={task.id.toString()} />
 			<label for={task.id.toString()}>
-				<input type="checkbox" checked={task.isDone} />{task.id}:
+				<input
+					type="checkbox"
+					checked={task.isDone}
+					on:change={async (e) => {
+						await toggleCompleted(task.id, e.currentTarget.checked);
+					}}
+				/>{task.id}:
 				<button>{task.title}</button>({task.createdAt})
 			</label>
 		</li>
