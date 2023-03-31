29c0affa0a200b234b23732a7a08995097ae7720
rvnkcode <sykim.p508@gmail.com>
2023-03-31 00:18:10 +0000

    Basic create operation

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,24 @@
 import { prisma } from '$lib/server/prisma';
-import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = (async () => {
 	return { tasks: await prisma.task.findMany() };
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	createTask: async ({ request }) => {
+		const { title } = Object.fromEntries(await request.formData()) as {
+			title: string;
+		};
+
+		try {
+			await prisma.task.create({
+				data: { title }
+			});
+		} catch (error) {
+			console.error(error);
+			return fail(500, { message: `Could not create the task` });
+		}
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,13 @@
 	$: ({ tasks } = data);
 </script>
 
+<form action="?/createTask" method="post">
+	<input type="text" name="title" required />
+	<button type="submit">submit</button>
+</form>
+
 <ul>
 	{#each tasks as task (task.id)}
-		<li>{task.title}</li>
+		<li>{task.id}: {task.title} ({task.createdAt}) {task.isDone}</li>
 	{/each}
 </ul>
