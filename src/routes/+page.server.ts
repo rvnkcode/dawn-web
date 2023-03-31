04b0017ffdf1544092ef622381c60d1b452b0078
rvnkcode <sykim.p508@gmail.com>
2023-03-31 07:03:07 +0000

    remove trash action

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -26,25 +26,5 @@ export const actions: Actions = {
 			console.error(error);
 			return fail(500, { message: `Could not create the task` });
 		}
-	},
-
-	trashSelectedTasks: async ({ request }) => {
-		const ids: number[] = Array.from((await request.formData()).keys()).map((e: string) => {
-			return +e;
-		});
-
-		try {
-			await prisma.task.updateMany({
-				where: {
-					id: { in: ids }
-				},
-				data: {
-					trash: true
-				}
-			});
-		} catch (error) {
-			console.error(error);
-			return fail(500, { message: `Could not trash the task` });
-		}
 	}
 } satisfies Actions;
