b2ec5bdc36b0be3513b7d891fa031220940aa730
rvnkcode <sykim.p508@gmail.com>
2023-04-05 09:24:22 +0000

    separate main footer component and update some styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import type { PageData } from './$types';
 	import List from '$lib/components/list.svelte';
 	import NewInput from '$lib/components/newInput.svelte';
+	import MainFooter from '$lib/components/mainFooter.svelte';
 
 	export let data: PageData;
 
@@ -29,15 +30,7 @@
 		{/if}
 	</main>
 
-	<footer>
-		<!-- TODO: When press ESC key, hide input -->
-		<!-- TODO: When click outside of input(out of focus), hide input -->
-		<button
-			on:click={() => {
-				showNewInput = !showNewInput;
-			}}><ion-icon name="add" /></button
-		>
-	</footer>
+	<MainFooter bind:value={showNewInput} />
 </section>
 
 <style>
@@ -45,6 +38,17 @@
 		max-width: 960px;
 		width: 100%;
 		margin: 0 auto;
+	}
+
+	@media (max-width: 480px) {
+		section {
+			padding: 0 1rem;
+		}
+
+		main {
+			/* Fixed footer's height */
+			margin-bottom: 3rem;
+		}
 	}
 
 	header {
@@ -69,61 +73,6 @@
 	}
 
 	main {
-		/* Fixed footer's height */
-		margin: 1rem 0 3rem 0;
-	}
-
-	footer {
-		position: fixed;
-		z-index: 1;
-		bottom: 0;
-		width: 100%;
-	}
-
-	footer > button {
-		border: none;
-		background: none;
-		width: 100%;
-		padding: 0.5rem 0;
-	}
-
-	@media (min-width: 481px) {
-		footer {
-			left: 0;
-			background-color: white;
-			display: flex;
-			justify-content: space-evenly;
-			/* Debug */
-			/* border: 2px solid burlywood; */
-		}
-
-		footer > button:hover {
-			cursor: pointer;
-			/* TODO: Change color */
-			border: 1px solid gray;
-			background-color: lightgray;
-		}
-	}
-
-	@media (max-width: 480px) {
-		section {
-			padding: 0 1rem;
-		}
-
-		footer {
-			/* TODO: Change color */
-			background-color: blue;
-			width: 3rem;
-			height: 3rem;
-			border-radius: 100%;
-			right: 0;
-			margin-right: 0.5rem;
-			margin-bottom: 0.5rem;
-		}
-
-		footer > button > ion-icon {
-			color: white;
-			font-size: xx-large;
-		}
+		margin-top: 2rem;
 	}
 </style>
