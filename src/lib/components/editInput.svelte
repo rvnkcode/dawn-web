5c2d274f6fcadec14343dd6a4626bbb1d4e43fb1
rvnkcode <sykim.p508@gmail.com>
2023-04-05 07:11:55 +0000

    svelte-ignore a11y-autofocus

--- src/lib/components/editInput.svelte
+++ src/lib/components/editInput.svelte
@@ -6,6 +6,7 @@
 
 <form action="?/updateTask" method="post">
 	<input type="hidden" value={task.id} name="id" />
+	<!-- svelte-ignore a11y-autofocus -->
 	<input type="text" bind:value={task.title} required autocomplete="off" name="title" autofocus />
 </form>
 
--- src/lib/components/newInput.svelte
+++ src/lib/components/newInput.svelte
@@ -1,4 +1,5 @@
 <form action="?/createTask" method="post">
+	<!-- svelte-ignore a11y-autofocus -->
 	<input type="text" name="title" required placeholder="New To-Do" autocomplete="off" autofocus />
 </form>
 
