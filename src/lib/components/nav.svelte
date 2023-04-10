d1f43ec3b1dddd3853e4f8a8f8860acbe1c7cc39
KSY <sykim.p508@gmail.com>
2023-04-10 12:10:32 +0000

    Separate nav component

---
+++ src/lib/components/nav.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: current = $page.url.pathname;
+</script>
+
+<nav>
+	<ul>
+		<li class={current === '/' ? 'current' : ''}>
+			<ion-icon name="file-tray" class="inbox" /><a href="/">Inbox</a>
+		</li>
+		<li class={current === '/trash' ? 'current' : ''}>
+			<ion-icon name="trash-bin" class="trashIcon" /><a href="/trash">Trash</a>
+		</li>
+	</ul>
+</nav>
+
+<style>
+	nav {
+		background-color: #f9fafb;
+		height: 100vh;
+		width: 100%;
+		max-width: 233px;
+		/* border: 2px solid red; */
+	}
+
+	ul {
+		margin: 3rem 1rem 0 1rem;
+	}
+
+	li {
+		margin-bottom: 0.25rem;
+		border-radius: 0.25rem;
+		padding: 0.4rem 0.25rem;
+	}
+
+	li.current {
+		background-color: #e4e7ea;
+	}
+
+	a {
+		text-decoration: none;
+		color: #292e34;
+		font-weight: bold;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,52 +1,13 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-
-	$: current = $page.url.pathname;
+<script>
+	import Nav from '$lib/components/nav.svelte';
 </script>
 
-<nav>
-	<ul>
-		<li class={current === '/' ? 'current' : ''}>
-			<ion-icon name="file-tray" class="inbox" /><a href="/">Inbox</a>
-		</li>
-		<li class={current === '/trash' ? 'current' : ''}>
-			<ion-icon name="trash-bin" class="trashIcon" /><a href="/trash">Trash</a>
-		</li>
-	</ul>
-</nav>
+<Nav />
 <section>
 	<slot />
 </section>
 
 <style>
-	nav {
-		background-color: #f9fafb;
-		height: 100vh;
-		width: 100%;
-		max-width: 233px;
-		/* border: 2px solid red; */
-	}
-
-	ul {
-		margin: 3rem 1rem 0 1rem;
-	}
-
-	li {
-		margin-bottom: 0.25rem;
-		border-radius: 0.25rem;
-		padding: 0.4rem 0.25rem;
-	}
-
-	li.current {
-		background-color: #e4e7ea;
-	}
-
-	a {
-		text-decoration: none;
-		color: #292e34;
-		font-weight: bold;
-	}
-
 	section {
 		max-width: 1020px;
 		width: 100%;
