fd8fbb68a5724c0ebeabadde96410315a7aeb2ff
rvnkcode <sykim.p508@gmail.com>
2023-04-26 06:24:31 +0000

    Remove unnecessary PATCH trash api

--- src/routes/api/list/+server.ts
+++ src/routes/api/list/+server.ts
@@ -2,17 +2,6 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma';
 
-// empty trash
-export const DELETE = (async () => {
-	await prisma.task.deleteMany({
-		where: {
-			trash: true
-		}
-	});
-
-	return json({}); // return nothing
-}) satisfies RequestHandler;
-
 // move to inbox
 export const PATCH = (async ({ request }) => {
 	const ids = await request.json();
