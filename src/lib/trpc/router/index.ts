9729d14857e5d25ea034b6d7402712a44249accd
rvnkcode <sykim.p508@gmail.com>
2023-04-26 06:48:39 +0000

    Update archive page load data to tRPC query

--- src/lib/trpc/router/index.ts
+++ src/lib/trpc/router/index.ts
@@ -1,4 +1,5 @@
 import { router } from '../trpc';
+import { archiveRouter } from './archive';
 import { inboxRouter } from './inbox';
 import { countRouter } from './nav';
 import { trashRouter } from './trash';
@@ -6,7 +7,8 @@ import { trashRouter } from './trash';
 export const appRouter = router({
 	inbox: inboxRouter,
 	count: countRouter,
-	trash: trashRouter
+	trash: trashRouter,
+	archive: archiveRouter
 });
 
 export type AppRouter = typeof appRouter;
