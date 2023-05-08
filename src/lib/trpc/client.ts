932c7fc50d9700a9bb929abaf4aeb8cb8d605614
rvnkcode <sykim.p508@gmail.com>
2023-05-08 01:35:39 +0000

    Add superjson transformer to tRPC client and server setting

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -1,5 +1,6 @@
 import type { AppRouter } from './router/index';
 import { createTRPCClient, type TRPCClientInit } from 'trpc-sveltekit';
+import superjson from 'superjson';
 
 let browserClient: ReturnType<typeof createTRPCClient<AppRouter>>;
 
@@ -7,7 +8,7 @@ export function trpc(init?: TRPCClientInit) {
 	const isBrowser = typeof window !== 'undefined';
 	if (isBrowser && browserClient) return browserClient;
 
-	const client = createTRPCClient<AppRouter>({ init });
+	const client = createTRPCClient<AppRouter>({ init, transformer: superjson });
 	if (isBrowser) browserClient = client;
 	return client;
 }
