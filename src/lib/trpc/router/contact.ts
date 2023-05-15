4c3d4d06bc7d27fa63d2372bbcd6a898a0b957f7
KSY <sykim.p508@gmail.com>
2023-05-15 11:58:15 +0000

    Update tRPC contact router
    - Query only activated contacts at input component
    - Query all contacts for preference

--- src/lib/trpc/router/contact.ts
+++ src/lib/trpc/router/contact.ts
@@ -3,6 +3,28 @@ import { prisma } from '$lib/server/prisma';
 
 export const contactRouter = router({
 	getContacts: publicProcedure.query(async () => {
-		return await prisma.contact.findMany();
+		return await prisma.contact.findMany({
+			where: {
+				activate: true
+			}
+		});
+	}),
+
+	getAllContacts: publicProcedure.query(async () => {
+		const [disabled, activated] = await Promise.all([
+			await prisma.contact.findMany({
+				where: {
+					activate: false
+				}
+			}),
+
+			await prisma.contact.findMany({
+				where: {
+					activate: true
+				}
+			})
+		]);
+
+		return { disabled, activated };
 	})
 });
