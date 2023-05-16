4cb3cb0cb353855fa9d80cc2b95ad17add4b11d2
rvnkcode <sykim.p508@gmail.com>
2023-05-16 00:33:52 +0000

    Add debug console.log

--- src/routes/archive/+page.server.ts
+++ src/routes/archive/+page.server.ts
@@ -4,5 +4,6 @@ import { createContext } from '$lib/trpc/context';
 
 export const load: PageServerLoad = async ({ cookies }) => {
 	const timeZone = cookies.get('timeZone');
+	// console.log(`timeZone on +page.server.ts(archive):${timeZone}`); // Debug
 	return appRouter.createCaller(await createContext()).archive.getArchive(timeZone ?? 'UTC');
 };
