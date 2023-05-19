0b1d581deda382caf7720daa28df0856c918ca38
rvnkcode <sykim.p508@gmail.com>
2023-05-19 08:08:13 +0000

    Add zod task create/update input schema

--- src/lib/zod/index.ts
+++ src/lib/zod/index.ts
@@ -1,3 +1,27 @@
 import * as z from 'zod';
 
 export const zPathEnum = z.enum(['/inbox', '/waiting_for']);
+
+export const zTaskCreateInput = z.object({
+	title: z.string(),
+	comments: z.string().transform((arg) => (arg?.length < 1 ? undefined : arg)),
+	rawUrls: z.string().optional(),
+	url: z.string().url().optional(),
+	allocatedTo: z
+		.string()
+		.optional()
+		.transform((arg) => (arg != null && arg?.length < 1 ? undefined : arg))
+});
+
+export const zTaskUpdateInput = z.object({
+	id: z.coerce.number().int(),
+	title: z.string(),
+	comments: z.string().transform((arg) => (arg?.length < 1 ? null : arg)),
+	rawUrls: z.string().optional(),
+	url: z.string().url().optional(),
+	allocatedTo: z
+		.string()
+		.optional()
+		.transform((arg) => (arg != null && arg?.length < 1 ? undefined : arg)),
+	completedAt: z.coerce.date().optional()
+});
