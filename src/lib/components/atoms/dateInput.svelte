0b0020e16b85bf00f51f077692d72a07278e1c3c
KSY <sykim.p508@gmail.com>
2023-05-22 12:30:48 +0000

    Convert completed at input to date input with started at property too

---
+++ src/lib/components/atoms/dateInput.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { format } from 'date-fns';
+
+	type DateKind = 'started' | 'completed';
+
+	export let date: Date | null = null;
+	export let attributeType: DateKind;
+
+	let value = '';
+
+	if (date) {
+		value = format(date, 'yyyy-MM-dd');
+	}
+
+	const today = format(new Date(), 'yyyy-MM-dd');
+
+	const handleLabel = (dateKind: DateKind) => {
+		switch (dateKind) {
+			case 'started':
+				return `When`;
+
+			case 'completed':
+				return `Completed`;
+		}
+	};
+
+	const handleName = (dateKind: DateKind) => {
+		switch (dateKind) {
+			case 'started':
+				return `startedAt`;
+
+			case 'completed':
+				return 'completedAt';
+		}
+	};
+</script>
+
+<label>
+	<span>{handleLabel(attributeType)}:</span>
+	<input
+		type="date"
+		bind:value
+		name={handleName(attributeType)}
+		max={attributeType == 'completed' ? today : null}
+	/>
+</label>
+
+<style>
+	input[type='date'] {
+		border: none;
+		margin: 0.5rem 0 0.25rem 0;
+	}
+</style>
