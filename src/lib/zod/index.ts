1c3f7c886580cc1928c0ef6fad94489e4eb09aea
rvnkcode <sykim.p508@gmail.com>
2023-05-23 12:16:22 +0000

    Add started and completion date validation in zod

--- src/lib/zod/index.ts
+++ src/lib/zod/index.ts
@@ -20,21 +20,27 @@ export const zTaskCreateInput = z.object({
 		.transform((arg) => (arg != null && arg?.length < 1 ? undefined : arg))
 });
 
-export const zTaskUpdateInput = z.object({
-	id: z.coerce.number().int(),
-	title: z.string(),
-	startedAt: z
-		.string()
-		.optional()
-		.transform((arg) =>
-			arg != null && arg?.length < 1 ? null : parse(arg ?? '', 'yyyy-MM-dd', new Date())
-		),
-	comments: z.string().transform((arg) => (arg?.length < 1 ? null : arg)),
-	rawUrls: z.string().optional(),
-	url: z.string().url().optional(),
-	allocatedTo: z
-		.string()
-		.optional()
-		.transform((arg) => (arg != null && arg?.length < 1 ? undefined : arg)),
-	completedAt: z.coerce.date().optional()
-});
+export const zTaskUpdateInput = z
+	.object({
+		id: z.coerce.number().int(),
+		title: z.string(),
+		startedAt: z
+			.string()
+			.optional()
+			.transform((arg) =>
+				arg != null && arg?.length < 1 ? null : parse(arg ?? '', 'yyyy-MM-dd', new Date())
+			),
+		comments: z.string().transform((arg) => (arg?.length < 1 ? null : arg)),
+		rawUrls: z.string().optional(),
+		url: z.string().url().optional(),
+		allocatedTo: z
+			.string()
+			.optional()
+			.transform((arg) => (arg != null && arg?.length < 1 ? undefined : arg)),
+		completedAt: z.coerce.date().optional()
+	})
+	// Condition of correct(what data should be)
+	.refine((data) => data.completedAt && data.startedAt && data.completedAt >= data.startedAt, {
+		message: 'Completion date cannot be earlier than the started date',
+		path: ['startedAt', 'completedAt']
+	});
