2369c7948d21b41a5575a22dccbc291b003c60f7
rvnkcode <sykim.p508@gmail.com>
2023-05-23 11:51:53 +0000

    Remove TODO comment

--- src/lib/components/organisms/nav.svelte
+++ src/lib/components/organisms/nav.svelte
@@ -7,7 +7,6 @@
 	export let count: RouterOutputs['count']['getCounts'];
 </script>
 
-<!-- TODO: Separate nav lists  -->
 <nav class={$showSidebar ? 'show' : ''}>
 	<ul class="container">
 		<NavSubList>
