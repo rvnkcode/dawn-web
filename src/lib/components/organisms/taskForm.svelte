670536684db0017c3d34cbf8deeee1be2b6037c1
KSY <sykim.p508@gmail.com>
2023-06-12 12:55:32 +0000

    Add new task creation form and validations

--- src/lib/components/organisms/taskForm.svelte
+++ src/lib/components/organisms/taskForm.svelte
@@ -1,29 +1,60 @@
 <script lang="ts">
-	import { Button, TextArea, TextInput } from 'carbon-components-svelte';
-	import { OverflowMenuVertical } from 'carbon-icons-svelte';
+	import { reporter, ValidationMessage } from '@felte/reporter-svelte';
+	import { validator } from '@felte/validator-zod';
+	import { Button, TextArea, TextInput, ToastNotification } from 'carbon-components-svelte';
+	import { Add, OverflowMenuVertical } from 'carbon-icons-svelte';
+	import { createForm } from 'felte';
 
+	import { invalidateAll } from '$app/navigation';
+	import { zTaskSchema } from '$lib/zod';
+	
 	import InputButtons from '../molecules/inputButtons.svelte';
 
 	let showMoreInputs = false;
+
+	const { form, reset } = createForm({
+		extend: [validator({ schema: zTaskSchema }), reporter],
+		onSuccess() {
+			invalidateAll();
+			reset();
+		}
+	});
 </script>
 
-<div>
-	<TextInput placeholder="New To-Do" size="sm" name="title" required autocomplete="off" />
-	<Button
-		size="small"
-		icon={OverflowMenuVertical}
-		iconDescription="More"
-		on:click={() => (showMoreInputs = !showMoreInputs)}
-	/>
-</div>
+<form use:form method="post" action={'/?/createTask'}>
+	<div>
+		<TextInput placeholder="New To-Do" size="sm" name="title" autocomplete="off" />
+		<Button
+			size="small"
+			icon={OverflowMenuVertical}
+			iconDescription="More"
+			kind="tertiary"
+			on:click={() => (showMoreInputs = !showMoreInputs)}
+		/>
+		<Button size="small" icon={Add} iconDescription="Add new To-DO" type="submit" />
+	</div>
 
-{#if showMoreInputs}
-	<TextArea placeholder="Notes" name="comments" />
-	<InputButtons />
-{/if}
+	<ValidationMessage for="title" let:messages={message}>
+		{#if message && message?.length > 0}
+			<ToastNotification subtitle={message.toString()} lowContrast class="notification" />
+		{/if}
+	</ValidationMessage>
+
+	{#if showMoreInputs}
+		<TextArea placeholder="Notes" name="comments" />
+		<InputButtons />
+	{/if}
+</form>
 
 <style>
 	div {
 		display: flex;
+	}
+
+	:global(.notification) {
+		position: absolute;
+		top: 0;
+		right: 1rem;
+		z-index: 3;
 	}
 </style>
