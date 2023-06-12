00b16b7013beeea0ec62653827ad6366a740f9d8
KSY <sykim.p508@gmail.com>
2023-06-12 12:57:17 +0000

    Set main content component's max width and align center

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import 'carbon-components-svelte/css/all.css';
-	
+
 	import { Content } from 'carbon-components-svelte';
 	import { onMount } from 'svelte';
-	
+
 	import MainHeader from '$lib/components/organisms/mainHeader.svelte';
 	import Nav from '$lib/components/organisms/nav.svelte';
 	import { clientWidth } from '$lib/stores';
-	
+
 	import type { LayoutServerData } from './$types';
 
 	// Initialize page data: Get counts of tasks from count tRPC router
@@ -26,7 +26,15 @@
 <div bind:clientWidth={$clientWidth}>
 	<MainHeader bind:isSideNavOpen />
 	<Nav {count} clientWidth={$clientWidth} bind:isOpen={isSideNavOpen} />
-	<Content>
+
+	<Content class="content">
 		<slot />
 	</Content>
 </div>
+
+<style>
+	:global(.content) {
+		max-width: 1020px;
+		margin: 3rem auto 0 auto !important;
+	}
+</style>
