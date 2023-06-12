cb7391ddef819dca136f708c995955981faf72f0
KSY <sykim.p508@gmail.com>
2023-06-12 12:58:04 +0000

    Re-writing zod task schema

--- src/lib/zod/index.ts
+++ src/lib/zod/index.ts
@@ -1,4 +1,3 @@
-import { parse } from 'date-fns';
 import * as z from 'zod';
 
 export const zPathEnum = z.enum([
@@ -13,51 +12,13 @@ export const zPathEnum = z.enum([
 ]);
 export type zPathType = z.infer<typeof zPathEnum>;
 
-export const zTaskCreateInput = z.object({
-	title: z.string(),
-	startedAt: z
-		.string()
-		.optional()
-		.transform((arg) =>
-			arg == undefined || (arg !== undefined && arg?.length < 1)
-				? null
-				: parse(arg, 'yyyy-MM-dd', new Date())
-		),
-	comments: z.string().transform((arg) => (arg?.length < 1 ? undefined : arg)),
-	rawUrls: z.string().optional(),
-	url: z.string().url().optional(),
-	allocatedTo: z
+export const zTaskSchema = z.object({
+	title: z.string().min(1),
+	comments: z
 		.string()
 		.optional()
 		.transform((arg) => (arg != null && arg?.length < 1 ? undefined : arg)),
-	status: z.string().optional()
+	startedAt: z.coerce.date().optional(),
+	url: z.string().url().optional(),
+	allocatedTo: z.string().min(1).optional()
 });
-
-export const zTaskUpdateInput = z
-	.object({
-		id: z.coerce.number().int(),
-		title: z.string(),
-		startedAt: z
-			.string()
-			.optional()
-			.transform((arg) =>
-				arg == undefined || (arg !== undefined && arg?.length < 1)
-					? null
-					: parse(arg, 'yyyy-MM-dd', new Date())
-			),
-		comments: z.string().transform((arg) => (arg?.length < 1 ? null : arg)),
-		rawUrls: z.string().optional(),
-		url: z.string().url().optional(),
-		allocatedTo: z
-			.string()
-			.optional()
-			.transform((arg) => (arg != null && arg?.length < 1 ? undefined : arg)),
-		completedAt: z.coerce.date().optional(),
-		status: z.string().optional()
-	})
-	.refine((data) => {
-		if (data.completedAt && data.startedAt && data.completedAt < data.startedAt) {
-			return false;
-		}
-		return true;
-	});
