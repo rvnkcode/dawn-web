2d1fb01e80b74d51df7621fd9c5d5ee17cd36164
rvnkcode <sykim.p508@gmail.com>
2023-06-14 07:18:26 +0000

    Temp: commented out main footer and list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import Header from '$lib/components/organisms/header.svelte';
 	import InputForm from '$lib/components/organisms/inputForm.svelte';
-	import List from '$lib/components/organisms/list.svelte';
-	import MainFooter from '$lib/components/organisms/mainFooter.svelte';
+	// import List from '$lib/components/organisms/list.svelte';
+	// import MainFooter from '$lib/components/organisms/mainFooter.svelte';
+	import { clientWidth } from '$lib/stores';
 
 	import type { PageServerData } from './$types';
 
+	// Initialize page server data
 	export let data: PageServerData;
-
 	$: ({ tasks } = data);
 
-	let showNewInput = false;
+	let showInput = false;
 </script>
 
 <svelte:head>
@@ -18,14 +19,14 @@
 </svelte:head>
 
 <Header>
-	<ion-icon name="file-tray" class="inbox title" /><span>Inbox</span>
+	<ion-icon name="file-tray" class="inbox title" /><span>Inbox {$clientWidth}</span>
 </Header>
 
-<main>
-	<List {tasks} {showNewInput} />
-	{#if showNewInput}
-		<InputForm bind:value={showNewInput} />
+<main bind:clientWidth={$clientWidth}>
+	<!-- <List {tasks} showNewInput={showInput} /> -->
+	{#if showInput}
+		<InputForm bind:value={showInput} />
 	{/if}
 </main>
 
-<MainFooter bind:value={showNewInput} />
+<!-- <MainFooter bind:value={showNewInput} /> -->
